23a30298e9de282a6b43a5ef7c05d0111f1b9732
Nitsu98 <agustinalejofernandezortuzar@gmail.com>
2024-03-14 23:54:56 -0300

    Entorno descargable y resolución adaptable a distintas orientaciones de pantalla

--- src/ClassroomExperiences.jsx
+++ src/ClassroomExperiences.jsx
@@ -1,6 +1,8 @@
 //@ts-nocheck
-import React from "react";
 import "./App.css";
+import "./style.css";
+import CEStyles from "./ClassroomExperiences.module.css";
+import React from "react";
 import ceroMasInfinitoImage from "./assets/images/classroom-experiences/3.0.paseando por el 0+infinito.jpg";
 import dibujandoFisicamente from "./assets/images/classroom-experiences/3.2.0.Dibujando físicamente.png";
 import dibujandoSimboloArbol from "./assets/images/classroom-experiences/2.Ananda y Yacon pintando arbol.jpg";
@@ -13,133 +15,54 @@ import diploma from "./assets/images/classroom-experiences/1.Diploma.jpg";
 function ClassroomExperiences() {
   return (
     <>
-      <div
-        className="Text"
-        style={{
-          fontSize: 80,
-          flex: 1,
-        }}
-      >
-        Experiencias en aula
-      </div>
-      <p
-        className="Text"
-        style={{
-          fontSize: 20,
-          flex: 1,
-          width: "80%",
-        }}
-      >
+      <div className={`Text ${CEStyles.MainTitle}`}>Experiencias en aula</div>
+      <p className={`Text ${CEStyles.Description}`}>
         Les compartimos algunas experiencias en aula que hemos realizado con
         niños y niñas de distintas edades.
       </p>
-      <p
-        className="Text"
-        style={{
-          fontSize: 20,
-          flex: 1,
-          width: "80%",
-        }}
-      >
+      {/* <p className={`Text ${CEStyles.Description}`}>
         ¿Tenés experiencias utilizando Representar que quieras compartir?
         Escribinos y las publicamos!
-      </p>
+      </p> */}
 
-      <h2
-        className="Text"
-        style={{
-          fontSize: 25,
-          flex: 1,
-          width: "80%",
-          textAlign: "left",
-        }}
-      >
-        Verano 2023 en los latorios del pabellón 0+Infinito de la facultad de
-        exactas de la UBA:
+      <h2 className={`Text ${CEStyles.Subtitle}`}>
+        Verano 2023 en los laboratorios del pabellón 0+Infinito de la facultad
+        de exactas de la UBA:
       </h2>
       <img src={ceroMasInfinitoImage} style={{ width: "80%" }} />
-      <p
-        className="Text"
-        style={{
-          fontSize: 20,
-          flex: 1,
-          width: "80%",
-        }}
-      >
+      <p className={`Text ${CEStyles.Description}`}>
         Blah blah blah blah! bla bla bla bla bla bla bla bla bla bla bla bla
         bla.
       </p>
       <img src={dibujandoFisicamente} style={{ width: "80%" }} />
-      <p
-        className="Text"
-        style={{
-          fontSize: 20,
-          flex: 1,
-          width: "80%",
-        }}
-      >
+      <p className={`Text ${CEStyles.Description}`}>
         Blah blah blah blah? Bla bla bla! bla bla bla bla bla bla bla bla bla
         bla.
       </p>
       <img src={dibujandoSimboloArbol} style={{ width: "80%" }} />
-      <p
-        className="Text"
-        style={{
-          fontSize: 20,
-          flex: 1,
-          width: "80%",
-        }}
-      >
+      <p className={`Text ${CEStyles.Description}`}>
         Blah blah blah blah bla bla bla bla bla bla bla bla bla bla bla bla bla.
       </p>
-      <div>
+      <div style={{ display: "flex", justifyContent: "center" }}>
         <img src={pensandoRobot} style={{ width: "50%" }} />
         <img src={robotAlLado} style={{ width: "38.6%" }} />
       </div>
-      <p
-        className="Text"
-        style={{
-          fontSize: 20,
-          flex: 1,
-          width: "80%",
-        }}
-      >
+      <p className={`Text ${CEStyles.Description}`}>
         Blah blah blah blah! bla bla bla bla bla bla bla bla bla bla bla bla
         bla.
       </p>
       <img src={controlandoRobot} style={{ width: "80%" }} />
-      <p
-        className="Text"
-        style={{
-          fontSize: 20,
-          flex: 1,
-          width: "80%",
-        }}
-      >
+      <p className={`Text ${CEStyles.Description}`}>
         BLAH! blah blah blah? bla bla bla bla bla bla bla bla bla bla bla bla
         bla.
       </p>
       <img src={bugRobot} style={{ width: "80%" }} />
-      <p
-        className="Text"
-        style={{
-          fontSize: 20,
-          flex: 1,
-          width: "80%",
-        }}
-      >
+      <p className={`Text ${CEStyles.Description}`}>
         Blah blah blah blah! bla bla bla bla bla bla bla bla bla bla bla bla
         bla.
       </p>
       <img src={diploma} style={{ width: "80%" }} />
-      <p
-        className="Text"
-        style={{
-          fontSize: 20,
-          flex: 1,
-          width: "80%",
-        }}
-      >
+      <p className={`Text ${CEStyles.Description}`}>
         Gracias a todos los que participaron!
       </p>
     </>
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,132 +1,69 @@
 //@ts-nocheck
 import "./App.css";
-import "./Home.css";
-import React from "react";
+import homeStyles from "./Home.module.css";
 import "./style.css";
+import React from "react";
 import Button from "./components/Button";
 import { useNavigate } from "react-router-dom";
-import hojaCarton from "./assets/images/fondo-textura-carton-blanca.jpg";
 
 const imageSmalltalkPath = "Representar.image";
 
 function Home() {
   const navigate = useNavigate();
 
-  const styles = {
-    cartonButton: {
-      backgroundImage: `url(${hojaCarton})`,
-      backgroundBlendMode: "multiply",
-      borderRadius: "20px 20px 20px 20px",
-      borderWidth: 0,
-      borderBottomWidth: "1vh",
-      borderColor: "grey",
-    },
-    header: {
-      height: "40vh",
-      display: "flex",
-      flexDirection: "column ",
-      alignItems: "center",
-      justifyContent: "center",
-    },
-    mainTitle: {
-      fontSize: "10vw",
-    },
-    introduction: {
-      fontSize: "2vw",
-      width: "65%",
-      textAlign: "center",
-    },
-    rowContainer1: {
-      display: "flex",
-      flexDirection: "row",
-      justifyContent: "center",
-      alignItems: "center",
-      height: "30vh",
-      width: "100%",
-    },
-    rowContainer2: {
-      display: "flex",
-      flexDirection: "row",
-      justifyContent: "space-around",
-      alignItems: "center",
-      height: "30vh",
-      width: "100%",
-    },
-    columnContainer: {
-      display: "flex",
-      flexDirection: "column",
-      height: "90%",
-      alignItems: "stretch",
-      justifyContent: "space-around",
-    },
-    tryOnlineButton: {
-      backgroundColor: `rgba(0, 100, 255, 0.7)`,
-      fontSize: "5vw",
-      width: "50%",
-      height: "25vh",
-    },
-
-    tinyButton: {
-      fontSize: "2vw",
-      padding: "10px 10px 10px 10px",
-      marginLeft: 5,
-      marginRight: 5,
-    },
-
-    middleButton: {
-      fontSize: "3vw",
-      height: "25vh",
-      width: "30%",
-      marginLeft: 5,
-      marginRight: 5,
-    },
-  };
-
   return (
     <>
-      <div style={styles.header}>
-        <div className="Text" style={styles.mainTitle}>
-          Representar
-        </div>
-        <div className="Text" style={styles.introduction}>
-          blah blah blah blah blah blah blah blah blah blah blah blah blah blah
-          blah blah blah blah blah blah blah blah blah blah blah blah blah blah
-          blah blah blah blah blah blah
+      <div className={homeStyles.Header}>
+        <div className={`Text ${homeStyles.MainTitle}`}>Representar</div>
+        <div className={`Text ${homeStyles.Introduction}`}>
+          Un lenguaje de programación para fortalecer la visión de la
+          programación como construcción de representaciones
         </div>
       </div>
-      <div style={styles.rowContainer1}>
+      <div className={homeStyles.RowContainer1}>
         <Button
-          className="Text CartonButton TryOnlineButton"
+          className={`Text ${homeStyles.CartonButton} ${homeStyles.TryOnlineButton}`}
           onClick={() => {
             window.location.href =
               "https://squeak.js.org/run/#image=https://raw.githubusercontent.com/agustincico/agustincico.github.io/master/" +
               imageSmalltalkPath;
           }}
         >
-          Probar online
+          Probalo online
+        </Button>
+        <text className={`Text ${homeStyles.Separator}`}>O</text>
+        <Button
+          className={`Text ${homeStyles.CartonButton} ${homeStyles.DownloadButton}`}
+          onClick={() => {
+            const link = document.createElement("a");
+            link.href =
+              "imagenSmalltalk/Representar Multiplataforma 12-3-2024.zip";
+            link.download = "Representar.zip";
+            link.click();
+          }}
+        >
+          Descargá la versión completa
         </Button>
       </div>
-      <div style={styles.rowContainer2}>
-        {/* <div style={styles.columnContainer}>
+      <div className={homeStyles.RowContainer2}>
+        {/* <div className="ColumnContainer">
           <Button
-            className="Text CartonButton"
+            className="Text CartonButton TinyButton"
             onClick={() => {
               navigate("/about");
             }}
             style={{
-              ...styles.tinyButton,
               backgroundColor: `rgba(0, 200, 0, 0.7)`,
             }}
           >
             Sobre el proyecto
           </Button>
           <Button
-            className="Text CartonButton"
+            className="Text CartonButton TinyButton"
             onClick={() => {
               navigate("/contact-us");
             }}
             style={{
-              ...styles.tinyButton,
               backgroundColor: `rgba(255, 0, 200, 0.7)`,
             }}
           >
@@ -134,37 +71,31 @@ function Home() {
           </Button>
         </div> */}
         <Button
-          className="Text CartonButton"
+          className={`Text ${homeStyles.CartonButton} ${homeStyles.MiddleButton}`}
           onClick={() => {
             navigate("/classroom-experiences");
           }}
-          style={{
-            ...styles.middleButton,
-            backgroundColor: `rgba(200, 200, 0, 0.7)`,
-          }}
         >
-          Experiencias en aula
+          Conocé sobre nuestra experiencia en un aula
         </Button>
-        {/* <div style={styles.columnContainer}>
+        {/* <div className="ColumnContainer">
           <Button
-            className="Text CartonButton"
+            className="Text CartonButton TinyButton"
             onClick={() => {
               navigate("/news-blog");
             }}
             style={{
-              ...styles.tinyButton,
               backgroundColor: `rgba(100, 0, 255, 0.7)`,
             }}
           >
             Blog de Novedades
           </Button>
           <Button
-            className="Text CartonButton"
+            className="Text CartonButton TinyButton"
             onClick={() => {
               navigate("/tutorials");
             }}
             style={{
-              ...styles.tinyButton,
               backgroundColor: `rgba(200, 0, 0, 0.7)`,
             }}
           >
